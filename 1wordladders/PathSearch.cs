9e924b0d16e225ea136e4eb54caefa0b658a2140
jonataneckeskog <jonataneckeskog@gmail.com>
2026-03-27 19:26:09 +0100

    Implement PathSearch

--- 1wordladders/PathSearch.cs
+++ 1wordladders/PathSearch.cs
@@ -2,20 +2,50 @@ namespace _1wordLadders;
 
 public static class PathSearch
 {
+    /// <summary>
+    /// Implementation of BFS, using WordLadder as the data structure. ShortestPath
+    /// returns the length of the path from startWord to endWord. If the path isn't found,
+    /// -1 is returned.
+    /// </summary>
     public static int ShortestPath(WordLadder wordLadder, string startWord, string endWord)
     {
-        Queue<string> wordsToCheck = new Queue<string>(wordLadder.WordGraph[startWord]);
+        if (startWord == endWord) return 0;
 
-        while (wordsToCheck.Count > 0)
+        Queue<string> queue = new Queue<string>();
+        Dictionary<string, int> distances = new Dictionary<string, int>();
+
+        queue.Enqueue(startWord);
+        distances.Add(startWord, 0);
+
+        while (queue.Count > 0)
         {
-            string word = wordsToCheck.Dequeue();
+            string currentWord = queue.Dequeue();
 
-            if (word == endWord)
+            // Return the recorded distance if endWord is found
+            if (currentWord == endWord)
             {
-                return 0;
+                return distances[currentWord];
+            }
+
+            // Look up the neighbors
+            if (wordLadder.WordGraph.TryGetValue(currentWord, out var neighbors))
+            {
+                foreach (string neighbor in neighbors)
+                {
+                    // Only process nodes not visited yet
+                    if (!distances.ContainsKey(neighbor))
+                    {
+                        // The distance is the current word's distance + 1
+                        distances.Add(neighbor, distances[currentWord] + 1);
+
+                        // Add the neighbor to the queue to be checked later
+                        queue.Enqueue(neighbor);
+                    }
+                }
             }
         }
 
-        return 0;
+        // No path found
+        return -1;
     }
 }
