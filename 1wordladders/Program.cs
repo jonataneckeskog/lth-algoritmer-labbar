637b5faa6454da14cc6413d7bac961d3b3f813b9
jonataneckeskog <jonataneckeskog@gmail.com>
2026-03-27 19:37:58 +0100

    Update Program to execute for check_solution.sh

--- 1wordladders/Program.cs
+++ 1wordladders/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _1wordLadders;
+
+var wordLadder = new WordLadder();
+
+// Read first line: N M
+string? firstLine = Console.ReadLine();
+if (firstLine == null) return;
+
+var parts = firstLine.Split(' ');
+int N = int.Parse(parts[0]);
+int M = int.Parse(parts[1]);
+
+// Read N words
+for (int i = 0; i < N; i++)
+{
+    string? word = Console.ReadLine();
+    if (word != null)
+    {
+        wordLadder.InsertWord(word.Trim());
+    }
+}
+
+// Read M queries and process them
+for (int i = 0; i < M; i++)
+{
+    string? queryLine = Console.ReadLine();
+    if (queryLine != null)
+    {
+        var queryParts = queryLine.Split(' ');
+        string start = queryParts[0];
+        string end = queryParts[1];
+
+        int distance = PathSearch.ShortestPath(wordLadder, start, end);
+        if (distance == -1)
+        {
+            Console.WriteLine("Impossible");
+        }
+        else
+        {
+            Console.WriteLine(distance);
+        }
+    }
+}
