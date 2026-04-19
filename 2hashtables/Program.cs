d62bfd3884849a1e2857c372215c659eec06684d
jonataneckeskog <jonataneckeskog@gmail.com>
2026-04-19 14:37:38 +0200

    Lab 2: Translate reference.py to a c# script

--- 2hashtables/Program.cs
+++ 2hashtables/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _2hashtables;
+
+// HashTable, currently just the standard library one
+var hashTable = new Dictionary<string, int>();
+
+// Read words from standard input
+using var reader = new StreamReader(Console.OpenStandardInput());
+int i = 0;
+string? line;
+
+while ((line = reader.ReadLine()) != null)
+{
+    string word = line.Trim();
+
+    bool isPresent = hashTable.ContainsKey(word);
+    bool removeIt = i % 16 == 0;
+
+    if (isPresent)
+    {
+        if (removeIt) hashTable.Remove(word);
+        else hashTable[word]++;
+    }
+    else if (!removeIt)
+    {
+        hashTable[word] = 1;
+    }
+
+    i++;
+}
+
+if (hashTable.Count > 0)
+{
+    string? bestWord = null;
+    int maxCount = -1;
+
+    foreach (var kvp in hashTable)
+    {
+        if (kvp.Value > maxCount)
+        {
+            maxCount = kvp.Value;
+            bestWord = kvp.Key;
+        }
+        else if (kvp.Value == maxCount)
+        {
+            if (string.CompareOrdinal(kvp.Key, bestWord) < 0)
+            {
+                bestWord = kvp.Key;
+            }
+        }
+    }
+
+    Console.WriteLine($"{bestWord} {maxCount}");
+}
---
+++ 2hashtables/run_tests.sh
@@ -0,0 +1,2 @@
+dotnet publish -c Release
+./check_solution.sh ./bin/Release/net10.0/publish/2hashtables
