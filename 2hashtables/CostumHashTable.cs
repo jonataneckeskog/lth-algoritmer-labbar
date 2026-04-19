b39fc0772ab3ad9a494b02cedef3f9b60fd1fb77
jonataneckeskog <jonataneckeskog@gmail.com>
2026-04-19 21:13:19 +0200

    Implement Add and Resize methods

--- 2hashtables/CostumHashTable.cs
+++ 2hashtables/CostumHashTable.cs
@@ -17,6 +17,64 @@ public class CostomHashTable
         _maxThreshold = 0.75F;
     }
 
+    public void Add(string key, int value)
+    {
+        AddInternal(new HashNode(key, value, null), _data);
+        _count++;
+        Resize();
+    }
+
+    public void Remove(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Get(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Resize the HashMap based on if the current usage is above or below the pre-determined thresholds.
+    /// If it's above, all data is transfered to an array of twice the size. If below, the new array is
+    /// half the size.
+    /// </summary>
+    private void Resize()
+    {
+        float currentUsage = _count / _capacity;
+
+        if (currentUsage < _minThreshold)
+        {
+            _capacity /= 2;
+
+        }
+        else if (currentUsage > _maxThreshold)
+        {
+            _capacity *= 2;
+        }
+        else return;
+
+        // Transfer data to a new array (ReHash).
+        HashNode[] newData = new HashNode[_capacity];
+
+        foreach (var node in _data)
+        {
+            AddInternal(node, newData);
+        }
+
+        _data = newData;
+    }
+
+    private int HashCode(string key)
+    {
+        return (key.GetHashCode() & 0x7FFFFFFF) % _capacity;
+    }
+
+    private void AddInternal(HashNode node, HashNode[] targetArray)
+    {
+        throw new NotImplementedException();
+    }
+
     private class HashNode
     {
         public string Key { get; set; }
