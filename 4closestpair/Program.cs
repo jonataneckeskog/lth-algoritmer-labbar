b05abc745503799a10f10679460d4759e1b9ecff
jonataneckeskog <jonataneckeskog@gmail.com>
2026-05-01 14:12:58 +0200

    Implement the full O(nlogn) approach

--- 4closestpair/Program.cs
+++ 4closestpair/Program.cs
@@ -19,8 +19,45 @@ Vector2Int[] people = scanner.AsEnumerable()
 /// SOLVE
 ///
 
-double smallestDistance = people
-    .SelectMany((p1, index) => people.Skip(index + 1).Select(p2 => p1.Distance(p2)))
+Array.Sort(people, (p1, p2) => p1.X.CompareTo(p2.X));
+
+double smallestDistance = Solve(people);
+
+double Solve(Span<Vector2Int> pts)
+{
+    if (pts.Length <= 3) return BruteForce(pts.ToArray());
+
+    int mid = pts.Length / 2;
+    int midX = pts[mid].X;
+
+    double dL = Solve(pts[..mid]);
+    double dR = Solve(pts[mid..]);
+    double d = Math.Min(dL, dR);
+
+    var strip = new List<Vector2Int>();
+    for (int i = 0; i < pts.Length; i++)
+    {
+        if (Math.Abs(pts[i].X - midX) < d) strip.Add(pts[i]);
+    }
+
+    // Sort the strip by Y
+    strip.Sort((a, b) => a.Y.CompareTo(b.Y));
+
+    // Check neighbors in the strip
+    for (int i = 0; i < strip.Count; i++)
+    {
+        for (int j = i + 1; j < strip.Count && (strip[j].Y - strip[i].Y) < d; j++)
+        {
+            d = Math.Min(d, strip[i].Distance(strip[j]));
+        }
+    }
+
+    return d;
+}
+
+double BruteForce(Vector2Int[] pts) => pts
+    .SelectMany((p1, i) => pts[(i + 1)..].Select(p2 => p1.Distance(p2)))
+    .DefaultIfEmpty(0)
     .Min();
 
 
