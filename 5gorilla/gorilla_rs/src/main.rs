b9003909a83096606fb910174b3135b556073592
jonataneckeskog <jonataneckeskog@gmail.com>
2026-05-01 21:48:47 +0200

    Implement first trial version (not working)

--- 5gorilla/gorilla_rs/src/main.rs
+++ 5gorilla/gorilla_rs/src/main.rs
@@ -35,8 +35,8 @@ fn main() {
     let q: usize = tokens.next().unwrap().parse().unwrap();
 
     let queries = (0..q).map(|_| {
-        let s1 = tokens.next().unwrap();
-        let s2 = tokens.next().unwrap();
+        let s1 = tokens.next().unwrap().as_bytes();
+        let s2 = tokens.next().unwrap().as_bytes();
         (s1, s2)
     });
 
@@ -44,7 +44,35 @@ fn main() {
     // --- SOLVE PROBLEM ---
     // ---------------------
 
-    for (_s1, _s2) in queries {
-        // RUN DP ALGORITHM HERE
+    let mut word_grid = [0i32; 3501 * 3501];
+
+    for (s1, s2) in queries {
+        // Calculate the values for the grid
+        for i in 0..s1.len() {
+            for j in 0..s2.len() {
+                let idx = i * 3501 + j;
+
+                let cost = cost_matrix[(s1[i] as usize * 256) + s2[j] as usize];
+                word_grid[idx] = cost;
+            }
+        }
+
+        // Path-find through the grid
+        let mut sum = 0;
+        let mut current_location = 0;
+        let directions = [1, 4096, 4096];
+        while (current_location / 4096) + 1 < s1.len() && (current_location % 4096) + 1 < s2.len() {
+            let (min_val, chosen_dir) = directions
+                .into_iter()
+                .map(|dir| (word_grid[current_location + dir], dir))
+                .min_by_key(|&(val, _dir)| val)
+                .unwrap();
+
+            sum += min_val;
+            current_location += chosen_dir;
+        }
+
+        // Output result
+        println!("{}", sum);
     }
 }
