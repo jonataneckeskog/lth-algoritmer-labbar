b5ccd0d441ff70f98be4276a7f08ac7140dae72b
jonataneckeskog <jonataneckeskog@gmail.com>
2026-05-01 14:52:16 +0200

    Change build to release build

--- 5gorilla/rs_tests.sh
+++ 5gorilla/rs_tests.sh
@@ -1,2 +1,2 @@
-cargo build --manifest-path gorilla_rs/Cargo.toml --target-dir gorilla_rs/target
-./check_solution.sh ./gorilla_rs/target/debug/gorilla_rs
+cargo build --release --manifest-path gorilla_rs/Cargo.toml --target-dir gorilla_rs/target
+./check_solution.sh ./gorilla_rs/target/release/gorilla_rs
