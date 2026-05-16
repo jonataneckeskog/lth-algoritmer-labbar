7f53c400f4d0df61122d4ff1cf91125b0e50d043
jonataneckeskog <jonataneckeskog@gmail.com>
2026-05-16 17:35:58 +0200

    Fix RailwayBFS to return the path of EDGES

--- 6railwayplanning/RailwayBFS.cs
+++ 6railwayplanning/RailwayBFS.cs
@@ -2,49 +2,57 @@ namespace _6railwayplanning;
 
 public static class RailwayBFS
 {
-    public static List<T> FindShortestPath<T>(Func<T, IEnumerable<T>> getRailways, T start, T end)
-        where T : notnull, IEquatable<T>
+    public static List<TEdge> FindShortestPath<TNode, TEdge>(
+        Func<TNode, IEnumerable<TEdge>> getAvailableEdges,
+        Func<TEdge, TNode, TNode> getOtherNode,
+        TNode start,
+        TNode end)
+        where TNode : notnull, IEquatable<TNode>
     {
         if (start.Equals(end)) return [];
 
-        Queue<T> queue = [];
-        Dictionary<T, T> parentEdges = [];
+        Queue<TNode> queue = new();
+        Dictionary<TNode, TEdge> edgeTo = new();
 
         queue.Enqueue(start);
 
         while (queue.Count > 0)
         {
-            T current = queue.Dequeue();
+            TNode current = queue.Dequeue();
 
             if (current.Equals(end))
             {
-                // Backtrack to build the path
-                List<T> path = [];
-                T backtrack = end;
+                // Backtrack to build the path of edges
+                List<TEdge> path = new();
+                TNode backtrack = end;
 
                 while (!backtrack.Equals(start))
                 {
-                    T edgeUsed = parentEdges[backtrack];
+                    TEdge edgeUsed = edgeTo[backtrack];
                     path.Add(edgeUsed);
 
-                    backtrack = parentEdges[edgeUsed];
+                    // Move backwards along the edge to the previous node
+                    backtrack = getOtherNode(edgeUsed, backtrack);
                 }
 
                 path.Reverse();
                 return path;
             }
 
-            foreach (T neighbor in getRailways(current))
+            // Iterate over edges
+            foreach (TEdge edge in getAvailableEdges(current))
             {
-                if (!parentEdges.ContainsKey(neighbor) && !neighbor.Equals(start))
+                TNode neighbor = getOtherNode(edge, current);
+
+                if (!edgeTo.ContainsKey(neighbor) && !neighbor.Equals(start))
                 {
-                    parentEdges.Add(neighbor, current);
+                    edgeTo.Add(neighbor, edge);
                     queue.Enqueue(neighbor);
                 }
             }
         }
 
-        // No valid path with available capacity was found
+        // No valid path found
         return [];
     }
 }
